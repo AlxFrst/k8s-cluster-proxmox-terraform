db16a2d133a00dd176c97bfded401d0704d24dc6
Alx <alexandreforestier1107@gmail.com>
2024-04-09 20:49:00 +0200

    Ajouter la configuration du backend S3 et les variables associées

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
 terraform {
+  backend "s3" {
+    endpoint   = var.backend_endpoint # Utilisez l'adresse IP ou le nom de domaine de votre instance MinIO
+    bucket     = var.backend_bucket
+    key        = var.backend_key
+    region     = "main"
+    access_key = var.backend_access_key
+    secret_key = var.backend_secret_key
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true # Important pour MinIO
+  }
   required_providers {
     proxmox = {
-      source = "Telmate/proxmox"
+      source  = "Telmate/proxmox"
       version = "3.0.1-rc1"
     }
   }
